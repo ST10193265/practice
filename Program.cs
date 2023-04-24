4453d1478c54b3dd8f3a2a25b39d79b80a6b6e9c
PaigeRussell <104575754+ST10193265@users.noreply.github.com>
2023-04-24 17:21:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace prog6221.poe.part1
+{
+    internal class Program
+    {
+     
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Welcome to the recipe app Journey enter 1 to continue");
+            int Enters = Convert.ToInt32(Console.ReadLine());
+            while (Enters==1)
+            {
+                Console.WriteLine("Enter the options number that you would like to continue with: \n1) Create recipe \n2) Display full recipe \n3) Clear all data \n4) End program") ;
+            int userOption = Convert.ToInt32(Console.ReadLine());
+            
+                if (userOption == 1)
+                {
+                   
+                }
+                else if (userOption == 2)
+                {
+                   
+                }
+                else if (userOption == 3)
+                {
+                  
+                }
+                else if (userOption == 4)
+                {
+                   
+
+                }
+            }
+        }
+    }
+}
